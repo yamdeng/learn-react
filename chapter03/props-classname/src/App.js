50e4af4717db25ed0486c4779cb78848cca2f9e5
yamdeng <yamdeng@gmail.com>
2024-02-10 18:15:48 +0900

    chapter03 props case add

--- chapter03/props-classname/src/App.js
+++ chapter03/props-classname/src/App.js
@@ -3,9 +3,10 @@ import BoxClass from "./components/BoxClass";
 import BoxFunction from "./components/BoxFunction";
 
 function App() {
+  const profile = { name: "안용성", email: "yamdeng@gmail.com", age: 40 };
   return (
     <div>
-      <BoxClass bg="bg-red" />
+      <BoxClass bg="bg-blue" profile={profile} />
       <BoxFunction bg="bg-yellow" />
     </div>
   );
--- chapter03/props-classname/src/components/BoxClass.jsx
+++ chapter03/props-classname/src/components/BoxClass.jsx
@@ -3,10 +3,22 @@ import React, { Component } from "react";
 class BoxClass extends Component {
   render() {
     const { props } = this;
-    const { bg } = props;
+    const { bg, profile } = props;
+    const { name, age, email } = profile;
     return (
       <div className={bg}>
         <h1>BoxClass</h1>
+        <br />
+        profile full info string : {JSON.stringify(profile)}
+        <br />
+        profile full info string case 2 : {`${name}-${email}`}
+        <br />
+        name : {name}
+        <br />
+        email : {email}
+        <br />
+        age : {age}
+        <br />
       </div>
     );
   }
