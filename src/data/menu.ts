a8e23b51e4b7801aaf7a2175696d78985fe10385
yamdeng <yamdeng@gmail.com>
2024-03-10 12:27:11 +0900

    1,2 example add

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -1,14 +1,24 @@
 import ExampleBasicFormOne from "../components/example/ExampleBasicFormOne.tsx";
+import ExampleBasicFormTwo from "../components/example/ExampleBasicFormTwo.tsx";
 import { Menu } from "../types/index.ts";
+
+// ExambleFormList.tsx : example/list
+// ExampleFormTable.tsx : example/table
+// ExampleFormNestedTable.tsx : example/table-nested
 
 const menu: Menu[] = [
   {
     title: "example",
     children: [
       {
-        title: "BasicFormExample",
+        title: "ExampleBasicFormOne",
         path: "example/basic-one-column",
         component: ExampleBasicFormOne,
+      },
+      {
+        title: "ExampleBasicFormTwo",
+        path: "example/basic-two-column",
+        component: ExampleBasicFormTwo,
       },
     ],
   },
