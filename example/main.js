3eab52892790e65076ae75d916f885fb70a90bab
yamdeng <yamdeng@gmail.com>
2024-04-30 11:13:39 +0900

    main.js 수정

--- example/main.js
+++ example/main.js
@@ -1,12 +1,15 @@
-const form = document.querySelector('form#refresh-form');
-const endpointInput = document.querySelector('form#refresh-form input#endpoint');
-const queryInput = document.querySelector('form#refresh-form input#query');
-const btn = document.querySelector('form#refresh-form button');
-const ctx = document.querySelector('#myChart canvas').getContext('2d');
+const form = document.querySelector("form#refresh-form");
+const endpointInput = document.querySelector(
+  "form#refresh-form input#endpoint"
+);
+const queryInput = document.querySelector("form#refresh-form input#query");
+const btn = document.querySelector("form#refresh-form button");
+const ctx = document.querySelector("#myChart canvas").getContext("2d");
 
-endpointInput.value = 'https://prometheus.demo.do.prometheus.io/';
+endpointInput.value = "http://prometheus.openlabs.io:9090/";
 
-queryInput.value = 'sum by (job) (go_gc_duration_seconds)';
+queryInput.value =
+  'spring_cloud_gateway_requests_seconds_sum{instance=~"192\\\\.168\\\\.87\\\\.108:18080", routeId=~"fico-admin-route", job=~"gateway"}/spring_cloud_gateway_requests_seconds_count{instance=~"192\\\\.168\\\\.87\\\\.108:18080", routeId=~"fico-admin-route", job=~"gateway"}';
 // queryInput.value = 'go_memstats_heap_objects';
 // queryInput.value = 'node_load1';
 
@@ -19,44 +22,46 @@ const start = -1 * 60 * 60 * 1000;
 const end = 0; // now
 
 const myChart = new Chart(ctx, {
-    type: 'line',
-    plugins: [ChartDatasourcePrometheusPlugin],
-    options: {
-        animation: {
-            duration: 0,
-        },
-        scales: {},
-        plugins: {
-            'datasource-prometheus': {
-                prometheus: {
-                    endpoint: endpointInput.value,
-                },
-                // query: ['node_load1', 'node_load5', 'node_load15'],
-                query: queryInput.value,
-                // query: customReq,
-                timeRange: {
-                    type: 'relative',
-                    start: start,
-                    end: end,
-                    // msUpdateInterval: 2000,
-                },
-            },
-        },
+  type: "line",
+  plugins: [ChartDatasourcePrometheusPlugin],
+  options: {
+    animation: {
+      duration: 0,
     },
+    scales: {},
+    plugins: {
+      "datasource-prometheus": {
+        prometheus: {
+          endpoint: endpointInput.value,
+        },
+        // query: ['node_load1', 'node_load5', 'node_load15'],
+        query: queryInput.value,
+        // query: customReq,
+        timeRange: {
+          type: "relative",
+          start: start,
+          end: end,
+          // msUpdateInterval: 2000,
+        },
+      },
+    },
+  },
 });
 
-
 function customReq(start, end, step) {
-    const url = `https://prometheus.demo.do.prometheus.io/api/v1/query_range?query=${encodeURIComponent(queryInput.value)}&start=${start.getTime() / 1000}&end=${end.getTime() / 1000}&step=${step}`;
-    const proxiedUrl = `https://cors-anywhere-chartjs-demo.herokuapp.com/${url}`;
-    return fetch(proxiedUrl)
-        .then(response => response.json())
-        .then(response => response['data']);
+  const url = `https://prometheus.demo.do.prometheus.io/api/v1/query_range?query=${encodeURIComponent(
+    queryInput.value
+  )}&start=${start.getTime() / 1000}&end=${end.getTime() / 1000}&step=${step}`;
+  const proxiedUrl = `https://cors-anywhere-chartjs-demo.herokuapp.com/${url}`;
+  return fetch(proxiedUrl)
+    .then((response) => response.json())
+    .then((response) => response["data"]);
 }
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    myChart.options.plugins['datasource-prometheus'].prometheus.endpoint = endpointInput.value;
-    myChart.options.plugins['datasource-prometheus'].query = queryInput.value;
-    myChart.update();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  myChart.options.plugins["datasource-prometheus"].prometheus.endpoint =
+    endpointInput.value;
+  myChart.options.plugins["datasource-prometheus"].query = queryInput.value;
+  myChart.update();
 });
