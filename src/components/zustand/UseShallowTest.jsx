09c6c183cdfbda6d005914b455a7ede28926b859
yamdeng <yamdeng@gmail.com>
2024-06-21 15:48:12 +0900

    computed 상세 case add

--- src/components/zustand/UseShallowTest.jsx
+++ src/components/zustand/UseShallowTest.jsx
@@ -1,13 +1,24 @@
 import { create } from "zustand";
 import { useShallow } from "zustand/react/shallow";
 
-const useMeals = create((set) => ({
+const useMeals = create((set, get) => ({
   papaBear: "large porridge-pot",
   mamaBear: "middle-size porridge pot",
   littleBear: "A little, small, wee pot",
   age1: 10,
   age2: 20,
+  profile: {
+    name: "yamdeng",
+    auth: "READ",
+  },
   changeAge1: (newAge) => set(() => ({ age1: newAge })),
+  changeProfileAuth: (newAuth) =>
+    set((state) => ({ profile: { ...state.profile, auth: newAuth } })),
+  getProfileAuth: () => {
+    const profile = get().profile;
+    const auth = profile.auth;
+    return auth;
+  },
 }));
 
 const BearNames = () => {
@@ -33,9 +44,31 @@ const BearAges = () => {
   return <div>{ageTotal}</div>;
 };
 
+const BearProfile = () => {
+  console.log("BearProfile render");
+
+  // 아래의 경우로 할 경우 적용 안됨
+  // const getProfileAuth = useMeals((state) => state.getProfileAuth);
+  // const profileAuth = getProfileAuth();
+
+  // 아래의 방식으로 사용하면 auth가 당연히 변경됨
+  // const profile = useMeals((state) => state.profile);
+
+  const profileAuth = useMeals(
+    useShallow((state) => (state.profile ? state.profile.auth : "NONE"))
+  );
+  return (
+    <div>
+      profileAuth : {profileAuth}
+      {/* <p>profile : {JSON.stringify(profile)}</p> */}
+    </div>
+  );
+};
+
 export default function UseShallowTest() {
   console.log("UseShallowTest render");
   const changeAge1 = useMeals((state) => state.changeAge1);
+  const changeProfileAuth = useMeals((state) => state.changeProfileAuth);
 
   const ageSuperTotal = useMeals((state) => state.age1 + state.age2 + 100);
 
@@ -52,11 +85,16 @@ export default function UseShallowTest() {
         <button onClick={chaneKey}>change key</button>
         <br />
         <button onClick={() => changeAge1(200)}>changeAge1</button>
+        <br />
+        <button onClick={() => changeProfileAuth("WRITE")}>
+          changeProfileAuth
+        </button>
       </p>
       <p>ageSuperTotal : {ageSuperTotal}</p>
       <div>
         <BearNames />
         <BearAges />
+        <BearProfile />
       </div>
     </div>
   );
