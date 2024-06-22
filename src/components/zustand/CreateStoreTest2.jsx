80ae809bfbe5c22feec84cc718ca501d82d96e6a
yamdeng <yamdeng@gmail.com>
2024-06-22 11:00:47 +0900

    createstore2 test add

--- src/components/zustand/CreateStoreTest2.jsx
+++ src/components/zustand/CreateStoreTest2.jsx
@@ -1,20 +1,16 @@
-import { createContext, useContext } from "react";
 import { useStore } from "zustand";
 import useTestStore2 from "../../store/useTestStore2";
-
-const StoreContext = createContext();
+import CreateStoreTest from "./CreateStoreTest";
 
 const Profile = function Profile() {
   console.log("Profile Component Render");
-
-  const store = useContext(StoreContext);
-  const changeProfile = useStore(store, (state) => state.changeProfile);
-  const profile = useStore(store, (state) => state.profile);
+  const changeProfile = useStore(useTestStore2, (state) => state.changeProfile);
+  const profile = useStore(useTestStore2, (state) => state.profile);
 
   return (
     <div>
       <p>profile : {profile ? JSON.stringify(profile) : "not profile"}</p>
-      <button onClick={() => changeProfile({ name: "ays" })}>
+      <button onClick={() => changeProfile({ name: "ays222" })}>
         changeProfile
       </button>
     </div>
@@ -25,9 +21,11 @@ export default function CreateStoreTest2() {
   console.log("CreateStoreTest2 render");
 
   return (
-    <StoreContext.Provider value={useTestStore2}>
-      createStore test : context
+    <>
+      createStore2test : context
       <Profile />
-    </StoreContext.Provider>
+      <hr />
+      <CreateStoreTest />
+    </>
   );
 }
