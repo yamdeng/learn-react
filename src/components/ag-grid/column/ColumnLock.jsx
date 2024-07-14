e261dc1bdfcc974afe56ddb37a3e7b16546d17e5
yamdeng <yamdeng@gmail.com>
2024-07-14 16:22:29 +0900

    cell style : 좌측 border 적용

--- src/components/ag-grid/column/ColumnLock.jsx
+++ src/components/ag-grid/column/ColumnLock.jsx
@@ -1,31 +1,38 @@
-import { useState, useEffect, useRef } from 'react';
-import { AgGridReact } from 'ag-grid-react';
+import { useState, useEffect, useRef } from 'react'
+import { AgGridReact } from 'ag-grid-react'
 
-import { getAgGridColumnListByListIndex, getTestData } from '../../../data/grid/example-data';
+import {
+  getAgGridColumnListByListIndex,
+  getTestData
+} from '../../../data/grid/example-data'
 
 export default function ColumnLock() {
-  
-  const gridRef = useRef();
-  const [rowData, setRowData] = useState([]);
-  const defaultColumnInfos = getAgGridColumnListByListIndex(16);
-  defaultColumnInfos[12].pinned = 'right';
-  defaultColumnInfos[13].pinned = 'right';
-  const [columnInfos] = useState(defaultColumnInfos);
+  const gridRef = useRef()
+  const [rowData, setRowData] = useState([])
+  const defaultColumnInfos = getAgGridColumnListByListIndex(16)
+  defaultColumnInfos[12].pinned = 'right'
+  defaultColumnInfos[13].pinned = 'right'
+  defaultColumnInfos[2].cellClassRules = {
+    'rag-green': params => (params.value ? true : false)
+  }
+  const [columnInfos] = useState(defaultColumnInfos)
 
   useEffect(() => {
     setRowData(getTestData())
   }, [])
 
   const onGridReady = () => {
-    console.log('onGridReady call');
+    console.log('onGridReady call')
   }
 
-  return <div className="ag-theme-quartz" style={{height: 500}}>
-    <AgGridReact
-       ref={gridRef}
-       rowData={rowData}
-       columnDefs={columnInfos}
-       onGridReady={onGridReady}
-   />
-  </div>;
+  return (
+    <div className='ag-theme-quartz' style={{ height: 500 }}>
+      <AgGridReact
+        ref={gridRef}
+        rowData={rowData}
+        columnDefs={columnInfos}
+        onGridReady={onGridReady}
+      />
+    </div>
+  )
 }
